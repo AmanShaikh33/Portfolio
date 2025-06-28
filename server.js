8e9eeaf8470687cb6d45728a53adb861404d9525
Aman Shaikh <amanshaikh7238@gmail.com>
2025-06-28 09:08:31 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
